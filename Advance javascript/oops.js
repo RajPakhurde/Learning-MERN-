445a23594c13fce8ce0aca2516ec3ee49a28a75a
Raj Pakhurde <rajofficial4803@gmail.com>
2024-01-09 13:28:08 +0530

    learn about oops / prototype inheritance in javascript

--- Advance javascript/oops.js
+++ Advance javascript/oops.js
@@ -50,8 +50,179 @@ func();
 
 *********************************************************************
 
+// inherite key values from other object
 
+var obj1 = {
+    key1: "value1",
+    key2: "value2"
+}
 
+var obj2 = Object.create(obj1);     * it is creating new object as well as we are seting obj2 __proto__ to obj1
+obj2.key3 = "value3";
+
+console.log(obj2);
+ 
+*********************************************************************
+
+// Prototype
+
+function hello() {
+    console.log("hello world");
+}
+
+// javascript function ====> function + object;   * we can treat function as an object as well.
+console.log(hello.name);        * it will print name of the function.
+
+// you can add your own properties
+hello.myOwnProperty = "very unique vlue";
+console.log(hello.myOwnProperty);
+
+// function provides more userfull properties.
+
+console.log(hello.prototype); // {}   * function provides free space called prototype in whihc we can add key value pairs.
+
+// only functions provide prototype property
+
+hello.prototype.abc = "abc";
+hello.prototype.xyz = "xyz";
+hello.prototype.sing = function () {
+    return "lalalla";
+};
+
+console.log(hello.prototype.sing());
+
+*********************************************************************
+
+// new 
+
+* it will create new object which is equal to 'this'   this = {}
+* it will return that object
+
+// constructor function
+function CreateUser(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName= lastName;
+}
+
+CreateUser.prototype.about = function() {
+    // code here
+}
+
+CreateUser.prototype.sing = function() {
+    // code here
+}
+
+const uer1 = new CreateUser('harsit', 'vashith');
+const uer2 = new CreateUser('harsit', 'vashith');
+
+console.log(user1);
+console.log(user1.about());
+
+*********************************************************************
+
+// class
+
+class CreateUser {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName= lastName;
+    }
+
+    about(){
+        //code here
+    }
+    sing(){
+        //code here
+    }
+}
+
+const uer1 = new CreateUser('harsit', 'vashith');
+const uer2 = new CreateUser('harsit', 'vashith');
+
+console.log(user1);
+console.log(user1.about());
+
+*********************************************************************
+
+// Inheritance
+
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    eat() {
+        return `${this.name} is eating`;
+    }
+
+    isCute() {
+        return true;
+    }
+}
+
+class Dog extends Animal {                  * extends keyword is use to inherite properties from parent class
+    constructor(name, age, speed) {         * super()   this keyword is use to call the parent class constructor
+        super(name, age);
+        this.speed = speed;
+    }
+
+    run() {
+        return `${this.name} is running at ${this.speed} kmph`;
+    }
+}
+
+*********************************************************************
+
+// getters and setters
+
+class Person {
+    constructor(firstName, lastName) {        
+        this.firstName = firstName
+        this.lastName = lastName;
+    }
+
+    get fullName() {                        * getters we use for accessing functions like properties.
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    set fullName(fullName) {               * setters we use to modified values 
+        const [firstName, lastName] = fullName.split(" ");
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+const person1 = new Person("raj", "pakhurde");
+console.log(person1.fullName());      * if we are not useing getter
+console.log(person1.fullName);      * if we are using getter
+
+person1.fullName = "harshit vashist";
+cosnole.log(person1.fullName);
+
+*********************************************************************
+
+// static methods and properties
+
+class Person {
+    constructor(firstName, lastName) {        
+        this.firstName = firstName
+        this.lastName = lastName;
+    }
+      
+    static classInfo() {
+        // code here
+    }
+
+    static xyzvariable = "dkfjdkf";
+}    
+
+*for static methods and static properties we don't have to create object of this class.
+
+Person.classInfo();
+console.log(Person.xyzvariable);       * we cannot access this static properties and methods using objects it will give an error.
+
+*********************************************************************
 */
 
 

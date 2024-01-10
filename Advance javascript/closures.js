ec2602577f532a91b137a78d0c7c79ad30a2ff6c
Raj Pakhurde <rajofficial4803@gmail.com>
2024-01-10 09:30:51 +0530

    learn closures in javascript

---
+++ Advance javascript/closures.js
@@ -0,0 +1,37 @@
+// Closures
+
+/*
+function printFullName(firstName, lastName) {
+    function printName() {
+        console.log(firstName, lastName);
+    }
+    return printName;
+}
+
+var ans = printFullName("harshit", "vashist");
+ans();
+
+
+
+
+function func() {
+    let count = 0;
+    return function () {
+        if(count === 0) {
+            console.log("hi, you called me !!");
+            count++;
+        } else {
+            console.log("mai already ek bar call ho chuka hoon.");
+        }
+        
+    }
+}
+const myfunc = func();
+myfunc();
+myfunc();
+
+const myfunc2 = func();
+myfunc2();
+myfunc2();
+
+*/

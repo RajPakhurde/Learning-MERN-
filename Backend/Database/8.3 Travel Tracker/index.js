60ed20b48951d0949b09f1cb001e75e076039517
Raj Pakhurde <rajofficial4803@gmail.com>
2024-01-26 20:43:12 +0530

    learn postgres sql queries

---
+++ Backend/Database/8.3 Travel Tracker/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from 'pg';
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  password: "RajPakhurde@12345",
+  database: "world",
+  port: 5432
+})
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+  try {
+    const result = await db.query("SELECT country_code FROM visited_countries");
+    let countries = [];
+    result.rows.forEach(country => {
+      countries.push(country.country_code);
+    });
+
+    res.render("index.ejs", { countries: countries, toal: countries.length });
+    db.end();
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+ 
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
